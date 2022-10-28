53fb23560d5505a61e96d45d93305e07480e46d5
Fedor <kurnosenkovf@mail.ru>
2022-10-28 20:27:59 +0400

    29

---
+++ 29/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("  Введите элементы массива");
+int[] a = FillArray();
+PrintArray(a);
+
+void PrintArray(int[] mass )
+{
+    int line = mass.Length;
+    int count = 0;
+    Console.WriteLine( " Элементы массива: ");
+    while(count < line)
+    {
+       Console.Write($"{mass[count]} ");
+       count++; 
+    }
+}
+
+int[] FillArray()
+{
+    Console.WriteLine(" Введите колличество элементов массива:");
+    int Length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[Length];
+    int index = 0;
+    while (index < Length)
+    {
+        array[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+   return array; 
+}
+
+
+
+
+
+
